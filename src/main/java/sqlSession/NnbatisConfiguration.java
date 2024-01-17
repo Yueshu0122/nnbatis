cc8b6bc317cd38c46ff1c8d36f3b720ae1760ad7
YutongNi <2731989n@student.gla.ac.uk>
2024-01-17 15:24:16 +0800

    mapper commit

--- src/main/java/sqlSession/NnbatisConfiguration.java
+++ src/main/java/sqlSession/NnbatisConfiguration.java
@@ -1,6 +1,9 @@
 package sqlSession;
 
+import config.Function;
+import config.MapperBean;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
@@ -8,6 +11,9 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class NnbatisConfiguration {
     private static ClassLoader loader =
@@ -74,5 +80,56 @@ public class NnbatisConfiguration {
             throw new RuntimeException(e);
         }
         return connection;
+    }
+
+    public MapperBean readMapper(String path){
+        MapperBean mapperBean = new MapperBean();
+
+        try {
+            InputStream stream = loader.getResourceAsStream(path);
+
+            SAXReader reader = new SAXReader();
+
+            Document document = reader.read(stream);
+
+            Element root = document.getRootElement();
+
+            String namespace = root.attributeValue("namespace").trim();
+
+            mapperBean.setInterfaceName(namespace);
+
+            Iterator iterator = root.elementIterator();
+
+            List<Function> list = new ArrayList<>();
+
+            while(iterator.hasNext()){
+                Element e = (Element) iterator.next();
+                Function function = new Function();
+                String sqlType = e.getName().trim();
+                String funcName = e.attributeValue("id").trim();
+                String resultType = e.attributeValue("resultType").trim();
+                String sql = e.getText().trim();
+
+                function.setFuncName(funcName);
+                function.setSql(sql);
+                function.setSqlType(sqlType);
+
+                Object newInstance = Class.forName(resultType).newInstance();
+                function.setResultType(newInstance);
+
+                list.add(function);
+
+
+            }
+
+            mapperBean.setFunctions(list);
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return mapperBean;
     }
 }
